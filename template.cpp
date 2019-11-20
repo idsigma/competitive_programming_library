121ae9dc9b2b13d04799c1a8a0d8b9dff432e33b
idsigma <dejima2357@gmail.com>
2019-11-20 23:56:12 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+template<class T> using vec = vector<T>;
+template<class T> using vvec = vector<vec<T>>;
+
+int main(){
+    
+}
